98618d1c5b5c63c7267896e274901100c367f4db
Trieu <tantrieuf31.database@gmail.com>
2025-03-16 22:03:02 +0700

    refactoring code

---
+++ resynap_core/test_call_all_dags.py
@@ -0,0 +1,21 @@
+
+from  api_utils.airflow_rest_api import AirflowAPI
+
+
+# Usage Example
+if __name__ == "__main__":
+    airflow = AirflowAPI()
+
+    # Trigger the DAG
+    dag_id = "cdp_profile_analytics"
+    params = {
+        "segment_id": "",
+        "service_params": {"param": "value5", "custom": {"key": "value"}}
+    }
+    
+    response = airflow.trigger_dag(dag_id, params)
+    print(response)
+
+    # Get DAG status
+    status = airflow.get_dag_status(dag_id)
+    print(status)
